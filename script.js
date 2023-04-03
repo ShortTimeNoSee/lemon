03da9c0897c524ab38b2e013b64ec89dbaa19316
Nicholas <97000198+ShortTimeNoSee@users.noreply.github.com>
2023-04-02 22:04:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,34 @@ let x = 200,
     dy = 0;
 
 function drawLemon() {
-    ctx.fillStyle = 'yellow';
+    // Yellow circle
+    const yellowGradient = ctx.createRadialGradient(x, y, 10, x, y, 20);
+    yellowGradient.addColorStop(0, '#fff');
+    yellowGradient.addColorStop(1, '#ffcc00');
+    ctx.fillStyle = yellowGradient;
     ctx.beginPath();
     ctx.arc(x, y, 20, 0, Math.PI * 2);
     ctx.fill();
-    ctx.fillStyle = 'green';
+
+    // Green stem
+    ctx.fillStyle = '#22aa22';
     ctx.beginPath();
     ctx.arc(x - 10, y - 10, 10, 0, Math.PI * 2);
     ctx.fill();
+
+    // Texture lines
+    ctx.strokeStyle = '#cc9900';
+    ctx.lineWidth = 3;
+    ctx.beginPath();
+    ctx.moveTo(x - 15, y);
+    ctx.lineTo(x - 5, y);
+    ctx.moveTo(x + 5, y);
+    ctx.lineTo(x + 15, y);
+    ctx.moveTo(x, y - 15);
+    ctx.lineTo(x, y - 5);
+    ctx.moveTo(x, y + 5);
+    ctx.lineTo(x, y + 15);
+    ctx.stroke();
 }
 
 function clearCanvas() {
@@ -31,13 +51,13 @@ setInterval(update, 10);
 
 document.addEventListener('keydown', (event) => {
     if (event.keyCode === 37) {
-        dx = -1;
+        dx = -4;
     } else if (event.keyCode === 38) {
-        dy = -1;
+        dy = -4;
     } else if (event.keyCode === 39) {
-        dx = 1;
+        dx = 4;
     } else if (event.keyCode === 40) {
-        dy = 1;
+        dy = 4;
     }
 });
 
