7a2df3fbba080ccf1b1d0ae045880960e0fbb76c
Nicholas <97000198+ShortTimeNoSee@users.noreply.github.com>
2023-04-03 20:37:33 -0700

    Update script.js
    Yoink

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ tomatoImg.src = 'a tomato.png';
 let clickCount = 0;
 
 function drawLemon() {
-	const img = (clickCount === 7) ? tomatoImg : lemonImg;
+	const img = (clickCount >=  7) ? tomatoImg : lemonImg;
 	ctx.drawImage(img, x - 20, y - 20, 40, 40);
 }
 
